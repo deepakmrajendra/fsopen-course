07454f46d86a9087c7b90de1dee63c175cc3835f
deepakmrajendra <deepakm.rajendra@gmail.com>
2025-01-19 10:07:56 +0000

    Update part1/unicafe/src/App.jsx for 1.8 unicafe step3 exercise

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,26 +9,54 @@ const Display = ({text, metric}) => {
   return <p>{text} {metric}</p>
 }
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+const Statistics = (props) => {
+
+  const { good, neutral, bad } = props.states;
+
+  // console.log("good", good)
+  // console.log("neutral", neutral)
+  // console.log("bad", bad)
 
   // Calculate stats dynamically based on current state
   const total = good + neutral + bad
   const average = ((good * 1) + (neutral * 0) + (bad * -1)) / (good + neutral + bad)
   const positive = (good / (good + neutral + bad)) * 100
 
-  console.log(total, average, positive)
+  // console.log(total, average, positive)
 
   const stats = [
-      { name: 'all', metric: total },
-      { name: 'average', metric: average },
-      { name: 'positive', metric: positive + ' %'},
+    { name: 'good', metric: good },
+    { name: 'neutral', metric: neutral },
+    { name: 'bad', metric: bad },
+    { name: 'all', metric: total },
+    { name: 'average', metric: average },
+    { name: 'positive', metric: positive + ' %'},
   ]
 
-  console.log(stats)
+  // console.log(stats)
+
+  return (
+    <>
+      <Display text={stats[0].name} metric={stats[0].metric} />
+      <Display text={stats[1].name} metric={stats[1].metric} />
+      <Display text={stats[2].name} metric={stats[2].metric} />
+      <Display text={stats[3].name} metric={stats[3].metric} />
+      <Display text={stats[4].name} metric={stats[4].metric} />
+      <Display text={stats[5].name} metric={stats[5].metric} />
+    </>
+  )
+
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const states = {good, neutral, bad}
+
+  // console.log(states)
 
   return (
     <div>
@@ -37,12 +65,7 @@ const App = () => {
       <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button onClick={() => setBad(bad + 1)} text="bad" />
       <Header text="statistics" />
-      <Display text="good" metric={good} />
-      <Display text="neutral" metric={neutral} />
-      <Display text="bad" metric={bad} />
-      <Display text={stats[0].name} metric={stats[0].metric} />
-      <Display text={stats[1].name} metric={stats[1].metric} />
-      <Display text={stats[2].name} metric={stats[2].metric} />
+      <Statistics states = {states} />
     </div> 
   )
 }
